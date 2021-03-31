a69dc6ecf4972df351da93b3d785e9694f55f6e0
carocollazo <67442372+carocollazo@users.noreply.github.com>
2021-03-31 16:46:32 -0400

    home page skeleton

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,55 @@
 import React from "react";
+import './App.css';
+// import Design from "./images/design-icon.jpeg";
+// import Construction from "./images/construction-icon.jpeg";
+// import Qualification from "./images/qualification-icon.jpeg";
+// import Maintenance from "./images/maintenance-icon.jpeg";
+// import Installation from "./images/installation-icon.jpeg";
 
 function Home() {
     return (
         <React.Fragment>
-            <h1>Home Page</h1>
+            <div className="homeBody">
+                <h1 className="homeHeadingText"><strong>Find Qualified Service Providers<br/>for your project.</strong></h1>
+                
+                <div className="serviceCategoriesContainer">
+                    <div className="serviceCategoryBox" href="">
+                        {/* <img className="serviceCategoryIcon" alt="designIcon" src={Design}></img> */}
+                        <p className="serviceCategoryName"><strong>Design</strong></p>
+                    </div>
+
+                    <div className="serviceCategoryBox" href="">
+                        {/* <img className="serviceCategoryIcon" alt="constructionIcon" src={Construction}></img>  */}
+                        <p className="serviceCategoryName"><strong>Construction</strong></p>
+                    </div>
+
+                    <div className="serviceCategoryBox" href="">
+                        {/* <img className="serviceCategoryImg" alt="qualificationIcon" src={Qualification}></img> */}
+                        <p className="serviceCategoryName"><strong>Qualification</strong></p>
+                    </div>
+
+                    <div className="serviceCategoryBox" href="">
+                        {/* <img className="serviceCategoryIcon" alt="maintenanceIcon" src={Maintenance}></img> */}
+                        <p className="serviceCategoryName"><strong>Maintenance</strong></p>
+                    </div>
+
+                    <div className="serviceCategoryBox" href="">
+                        {/* <img className="serviceCategoryIcon" alt="installationIcon" src={Installation}></img> */}
+                        <p className="serviceCategoryName"><strong>Installation</strong></p>
+                    </div>
+                </div>
+                
+                <div className="companyCategoriesContainer">
+                    <div className="companyCategoryBox" href="">
+                        {/* <img className="companyLogo" alt="companyLogo" src={}></img> */}
+                        <p className="companyCategoryName"><strong>Buyers</strong></p>
+                    </div>
+                    <div className="companyCategoryBox" href="">
+                        {/* <img className="companyLogo" alt="companyLogo" src={}></img> */}
+                        <p className="companyCategoryName"><strong>Sellers</strong></p>
+                    </div>
+                </div>
+            </div>  
         </React.Fragment>
     )    
 }
