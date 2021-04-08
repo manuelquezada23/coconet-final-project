5367396a1b27edb26cbc43cb744be854e17ad9e7
carocollazo <67442372+carocollazo@users.noreply.github.com>
2021-04-07 22:33:42 -0400

    Home.js complete?

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './App.css';
-import PlaceHolder from "./images/placeholder.webp";
 // import Design from "./images/design-icon.jpeg";
 // import Construction from "./images/construction-icon.jpeg";
 // import Qualification from "./images/qualification-icon.jpeg";
@@ -17,54 +16,58 @@ function Home() {
                 <h1 className="bodyHeadingText"><strong>Find Qualified Service Providers<br/>for your project.</strong></h1>
                 
                 <div className="serviceCategoriesContainer">
-                    <div className="serviceCategoryBox" href="">
-                        <img className="serviceCategoryIcon" alt="designIcon" src={PlaceHolder}/>
+                    <a className="serviceCategoryBox" href="http://google.com">
+                            <img className="serviceCategoryIcon" alt="designIcon" src="https://static.thenounproject.com/png/75540-200.png"/>
                         <p className="serviceCategoryName"><strong>Design</strong></p>
-                    </div>
+                    </a>
 
-                    <div className="serviceCategoryBox" href="">
-                        <img className="serviceCategoryIcon" alt="constructionIcon" src={PlaceHolder}/>
+                    <a className="serviceCategoryBox" href="http://google.com">
+                            <img className="serviceCategoryIcon" alt="constructionIcon" src="https://www.freeiconspng.com/thumbs/building-icon/office-building-icon-32.png"/>
                         <p className="serviceCategoryName"><strong>Construction</strong></p>
-                    </div>
+                    </a>
 
-                    <div className="serviceCategoryBox" href="">
-                        <img className="serviceCategoryIcon" alt="qualificationIcon" src={PlaceHolder}/>
+                    <a className="serviceCategoryBox" href="http://google.com">
+                            <img className="serviceCategoryIcon" alt="qualificationIcon" src="https://www.pngarts.com/files/11/Qualification-Ribbon-PNG-Image-Background.png"/>
                         <p className="serviceCategoryName"><strong>Qualification</strong></p>
-                    </div>
+                    </a>
 
-                    <div className="serviceCategoryBox" href="">
-                        <img className="serviceCategoryIcon" alt="maintenanceIcon" src={PlaceHolder}/>
+                    <a className="serviceCategoryBox" href="http://google.com">
+                            <img className="serviceCategoryIcon" alt="maintenanceIcon" src="https://www.pngkey.com/png/full/436-4368638_tools-transparent-tools-icon-png.png"/>
                         <p className="serviceCategoryName"><strong>Maintenance</strong></p>
-                    </div>
+                    </a>
 
-                    <div className="serviceCategoryBox" href="">
-                        <img className="serviceCategoryIcon" alt="installationIcon" src={PlaceHolder}/>
+                    <a className="serviceCategoryBox" href="http://google.com">
+                            <img className="serviceCategoryIcon" alt="installationIcon" src="https://image.flaticon.com/icons/png/512/81/81020.png"/>
                         <p className="serviceCategoryName"><strong>Installation</strong></p>
-                    </div>
+                    </a>
                 </div>
                 
                 <div className="companyCategoriesContainer">
-                    <div className="companyCategoryBox" href="">
-                        <button className="arrowButton">
+                    <div className="companyCategoryBox">
+                        <button className="arrowButton" onClick={() => {}}>
                             <img className="arrowImg" alt="leftArrow" src={LeftArrow}/>
                         </button>
-                        <button className="currentCompanyBox">
-                            <img className="companyLogo" alt="companyLogo" src={PlaceHolder}/>
+                        <div className="currentCompanyBox" href="google.com">
+                            <a href="/service-provider_id/about">
+                                <img className="companyLogo" alt="companyLogo" src="https://livejones.com/wp-content/uploads/2020/05/logo-Placeholder.png"/>
+                            </a>
                             <p className="companyCategoryName"><strong>Buyers</strong></p>
-                        </button>
-                        <button className="arrowButton">
+                        </div>
+                        <button className="arrowButton" onClick={() => {}}>
                             <img className="arrowImg" alt="rightArrow" src={RightArrow}/>
                         </button>
                     </div>
-                    <div className="companyCategoryBox" href="">
-                        <button className="arrowButton">
+                    <div className="companyCategoryBox">
+                        <button className="arrowButton" onClick={() => {}}>
                             <img className="arrowImg" alt="leftArrow" src={LeftArrow}/>
                         </button>
-                        <button className="currentCompanyBox">
-                            <img className="companyLogo" alt="companyLogo" src={PlaceHolder}/>
+                        <div className="currentCompanyBox">
+                            <a href="/service-provider_id/about">
+                                <img className="companyLogo" alt="companyLogo" src="https://livejones.com/wp-content/uploads/2020/05/logo-Placeholder.png"/>
+                            </a>
                             <p className="companyCategoryName"><strong>Sellers</strong></p>
-                        </button>
-                        <button className="arrowButton">
+                        </div>
+                        <button className="arrowButton" onClick={() => {}}>
                             <img className="arrowImg" alt="rightArrow" src={RightArrow}/>
                         </button>
                     </div>
