1ee4e3579c73e00cb61cdcf24160662ff4c443b9
manuelquezada23 <manuel_quezada@brown.edu>
2021-04-15 00:46:18 -0400

    navigation bar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,65 +1,75 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
 import LogoImage from "./images/coconet-logo-image.png"
 import SearchIcon from "./images/searchIcon.png"
 import Placeholder from "./images/profile-placeholder.png"
+import AuthService from "./services/auth.service";
 
-function NavigationBar() {
-    const loggedIn = false;
-    function sendToPage(link) {
+export default class NavigationBar extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          currentUser: AuthService.getCurrentUser()
+        };
+      }
+
+    sendToPage(link) {
         window.location.href = link;
     }
-    if (loggedIn) {
-        return (
-            <React.Fragment>
-                <div className="navigationBar">
-                    <img id="navigationBarLogo" src={LogoImage} onClick={() => {sendToPage("/")}}></img>
-                    <div id="navigationBarLogoText" onClick={() => {sendToPage("/")}}></div>
-                    <div id="searchBar">
-                            <select name="searchValue" id="searchBarDropdown">
-                                <option value="serviceProviders">Service Providers</option>
-                                <option value="projects">Projects</option>
-                            </select>
-                        <div id="line"></div>
-                        <input autocomplete="off" id="searchInputText" type="text" name="email" />
-                        <div id="line2"></div>
-                        <img id="searchIcon" alt="searchIcon" src={SearchIcon}></img>
-                    </div>
-                    <div id="loggedInMenu">
-                        <div className="loggedInDashboard" onClick={() => {sendToPage("/settings-profile")}}>
-                            <img className="loggedInPicture" src={Placeholder}></img>
-                            <p className="loggedInName">Name</p>
+    render() {
+        const { currentUser } = this.state;
+        if (currentUser) {
+            return (
+                <React.Fragment>
+                    <div className="navigationBar">
+                        <img id="navigationBarLogo" src={LogoImage} onClick={() => {this.sendToPage("/")}}></img>
+                        <div id="navigationBarLogoText" onClick={() => {this.sendToPage("/")}}></div>
+                        <div id="searchBar">
+                                <select name="searchValue" id="searchBarDropdown">
+                                    <option value="serviceProviders">Service Providers</option>
+                                    <option value="projects">Projects</option>
+                                </select>
+                            <div id="line"></div>
+                            <input autocomplete="off" id="searchInputText" type="text" name="email" />
+                            <div id="line2"></div>
+                            <img id="searchIcon" alt="searchIcon" src={SearchIcon}></img>
+                        </div>
+                        <div id="loggedInMenu">
+                            <div className="loggedInDashboard" onClick={() => {this.sendToPage("/settings-profile")}}>
+                                <img className="loggedInPicture" src={Placeholder}></img>
+                                <p className="loggedInName">{currentUser.username}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </React.Fragment>
-        ) 
-    } else {
-        return (
-            <React.Fragment>
-                <div className="navigationBar">
-                    <img id="navigationBarLogo" src={LogoImage} onClick={() => {sendToPage("/")}}></img>
-                    <div id="navigationBarLogoText" onClick={() => {sendToPage("/")}}></div>
-                    <div id="searchBar">
-                            <select name="searchValue" id="searchBarDropdown">
-                                <option value="serviceProviders">Service Providers</option>
-                                <option value="projects">Projects</option>
-                            </select>
-                        <div id="line"></div>
-                        <input autocomplete="off" id="searchInputText" type="text" name="email" />
-                        <div id="line2"></div>
-                        <img id="searchIcon" alt="searchIcon" src={SearchIcon} onClick={() => {sendToPage("/search")}}></img>
-                    </div>
-                    <div id="navBarButtons">
-                        <div className="userButtons">
-                            <button className="logInButton" onClick={() => {sendToPage("/login")}}>Log In</button>
-                            <button className="registerButton" onClick={() => {sendToPage("/signup")}}>Register</button>
+                </React.Fragment>
+            ) 
+        } else {
+            return (
+                <React.Fragment>
+                    <div className="navigationBar">
+                        <img id="navigationBarLogo" src={LogoImage} onClick={() => {this.sendToPage("/")}}></img>
+                        <div id="navigationBarLogoText" onClick={() => {this.sendToPage("/")}}></div>
+                        <div id="searchBar">
+                                <select name="searchValue" id="searchBarDropdown">
+                                    <option value="serviceProviders">Service Providers</option>
+                                    <option value="projects">Projects</option>
+                                </select>
+                            <div id="line"></div>
+                            <input autocomplete="off" id="searchInputText" type="text" name="email" />
+                            <div id="line2"></div>
+                            <img id="searchIcon" alt="searchIcon" src={SearchIcon}></img>
                         </div>
-                        <button className="serviceProviderButton" onClick={() => {sendToPage("/login-service-provider")}}>For Service Providers</button>
+                        <div id="navBarButtons">
+                            <div className="userButtons">
+                                <button className="logInButton" onClick={() => {this.sendToPage("/login")}}>Log In</button>
+                                <button className="registerButton" onClick={() => {this.sendToPage("/signup")}}>Register</button>
+                            </div>
+                            <button className="serviceProviderButton" onClick={() => {this.sendToPage("/login-service-provider")}}>For Service Providers</button>
+                        </div>
                     </div>
-                </div>
-            </React.Fragment>
-        ) 
-    }   
+                </React.Fragment>
+            ) 
+        }
+    }
 }
-export default NavigationBar;
