1806c0543c308edc5c277f20012e8af560158791
carocollazo <67442372+carocollazo@users.noreply.github.com>
2021-04-20 03:21:59 -0400

    SP almost done

--- src/SP.js
+++ src/SP.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import validator from 'validator' 
 import './App.css';
 
 export default class SP extends Component {
@@ -6,34 +7,90 @@ export default class SP extends Component {
         super(props);
         this.onChangeName = this.onChangeName.bind(this);
 
-        this.index = 0;
-    
-        this.about = {
+        this.projectIndex = 0;
+        this.clientIndex = 0;
+        this.qualificationIndex = 0;
+        
+        this.about = [
             // currentUser: AuthService.getCurrentUser()
-            name: "SP Name",
-            location: "Location",
-            type: "Type",
-            description: "description description description description description",
-        };
+            {name: ""},
+            {location: ""},
+            {type: ""},
+            {description: ""},
+        ];
 
         this.projects = [
-            {projectId: 0,
+            {projectId: this.projectIndex,
             projectName: "",
             projectDate: Number,
             projectPdf: ""}
         ];
 
-        this.contact = {
-            phoneNumber: "(XXX)XXX-XXXX",
-            email: "example@sp.com",
-            website: "example.com"
-        };
+        this.previousClients = [
+            {clientId: this.clientIndex,
+            clientName: "",
+            clientLocation: "",
+            clientWebsite: ""}
+        ];
+
+        this.qualifications = [
+            {qualificationId: this.qualificationIndex,
+            qualificationName: "",
+            qualificationDate: Number,
+            qualificationPdf: ""}
+        ];
+
+        this.contact = [
+            {phoneNumber: "(XXX)XXX-XXXX"},
+            {email: "example@sp.com"},
+            {website: "example.com"}
+        ];
     }
 
     onChangeName(e) {
-        this.setState((about) => {
-          return {name: e.target.value};
-        });
+        this.about[0] = e.target.value;
+    }
+
+    onChangeLocation(e) {
+        this.about[1] = e.target.value;
+    }
+
+    onChangeType(e) {
+        this.about[2] = e.target.value;
+    }
+
+    onChangeDescription(e) {
+        this.about[3] = e.target.value;
+    }
+
+    onChangeProjectName(e, index) {
+        this.projects[index].projectName = e.target.value;
+    }
+
+
+    onChangePhoneNumber(e) {
+        if (validator.isMobilePhone(e.target.value)) {
+            this.contact[0] = e.target.value;
+        } else {
+            // note that its not valid phone number
+            console.log('Phone number format is incorrect');
+        }
+    }
+
+    onChangeEmail(e) {
+        // if email is valid
+            this.contact[1] = e.target.value;
+        // else 
+            // note that its not valid email
+            console.log('Email format is incorrect');
+    }
+
+    onChangeWebsite(e) {
+        // if url is valid
+            this.contact[2] = e.target.value;
+        // else 
+            // note that its not valid url
+            console.log('Url format is incorrect');
     }
 
     addProject(e) {
@@ -47,19 +104,54 @@ export default class SP extends Component {
         colDate.appendChild(document.createElement('input'));
         colPdf.appendChild(document.createElement('input'));
         
-        this.index++;
-        const project = {projectId: this.index, projectName: "", projectDate: 0,};
+        this.projectIndex++;
+        const project = {projectId: this.projectIndex, projectName: "", projectDate: 0,};
         this.projects.push(project);
 
-        newRow.value = this.index;
-        console.log(newRow.value);
+        newRow.value = this.projectIndex;
     }
 
     removeProject(e) {
         e.preventDefault();
-        this.index++;
-        const project = {projectId: this.index, projectName: "", projectDate: 0,};
+        this.projectIndex--;
+        const project = {projectId: this.projectIndex, projectName: "", projectDate: 0,};
         this.projects.push(project);
+    }
+
+    addClient(e) {
+        e.preventDefault();
+        const newRow = document.getElementById('previousClientsSettings').appendChild(document.createElement('tr'));
+        const colName = newRow.insertCell(0);
+        const colDate = newRow.insertCell(1);
+        const colPdf = newRow.insertCell(2);
+
+        colName.appendChild(document.createElement('input'));
+        colDate.appendChild(document.createElement('input'));
+        colPdf.appendChild(document.createElement('input'));
+        
+        this.previousClientIndex++;
+        const project = {projectId: this.previousClientIndex, projectName: "", projectDate: 0,};
+        this.projects.push(project);
+
+        newRow.value = this.previousClientIndex;
+    }
+
+    addQualification(e) {
+        e.preventDefault();
+        const newRow = document.getElementById('qualificationsSettings').appendChild(document.createElement('tr'));
+        const colName = newRow.insertCell(0);
+        const colDate = newRow.insertCell(1);
+        const colPdf = newRow.insertCell(2);
+
+        colName.appendChild(document.createElement('input'));
+        colDate.appendChild(document.createElement('input'));
+        colPdf.appendChild(document.createElement('input'));
+        
+        this.qualificationIndex++;
+        const qualification = {qualificationId: this.qualificationIndex, qualificationName: "", qualificationDate: 0,};
+        this.qualifications.push(qualification);
+
+        newRow.value = this.qualificationIndex;
     }
 
     sendToPage(link) {
@@ -75,7 +167,7 @@ export default class SP extends Component {
                     <div id="settingsContent" className='settingsContent'>
                         <h2>About</h2>
                         <form>
-                            <table id="aboutSettings">
+                            <table className="aboutForm" id="aboutSettings">
                                 <tbody>
                                     <tr>
                                         <td className="settignsCategoryPromt"><b> Company Name: </b></td>
@@ -83,8 +175,7 @@ export default class SP extends Component {
                                                 id="companyName"
                                                 type="text"
                                                 name="name"
-                                                value={this.about.name}
-                                                onChange={this.onChangeName}
+                                                onChange={e => {this.onChangeName(e)}}
                                             />
                                         </td>
                                     </tr>
@@ -94,15 +185,15 @@ export default class SP extends Component {
                                                 id="companyLocation"
                                                 type="text"
                                                 name="location"
-                                                value={this.about.location}
+                                                onChange={e => {this.onChangeLocation(e)}}
                                             />
                                         </td>
                                     </tr>
                                     <tr>
                                         <td className="settignsCategoryPromt"><b> Service Type: </b></td>
                                         <td className="serviceTypeInputBox">
-                                            <select className="serviceTypeDropdown" id="serviceType">
-                                                <option defaultValue> {this.about.type} </option>
+                                            <select className="serviceTypeDropdown" id="serviceType" onChange={e => {this.onChangeType(e)}}>
+                                                <option defaultValue> </option>
                                                 <option> Design </option>
                                                 <option> Construction </option>
                                                 <option> Qualification </option>
@@ -117,7 +208,7 @@ export default class SP extends Component {
                                                 id="description"
                                                 type="text"
                                                 name="description"
-                                                value={this.about.description}
+                                                onChange={e => {this.onChangeDescription(e)}}
                                             />
                                         </td>
                                     </tr>
@@ -130,49 +221,148 @@ export default class SP extends Component {
                         <form>
                             <table id="projectsSettings">
                                 <thead>
-                                    <td className="projectNameInput"><b>Project Name</b></td>
-                                    <td className="projectDateInput"><b>Date</b></td>
-                                    <td className="projectPdfInput"><b>PDF</b></td>
-                                </thead>
-                                <tbody>
                                     <tr>
-                                        <td><input
-                                                value={this.projects.projectName}
+                                        <td className="projectName"><b>Project Name</b></td>
+                                        <td className="projectDate"><b>Date</b></td>
+                                        <td className="projectPdf"><b>PDF</b></td>
+                                    </tr>
+                                </thead>
+                                <tbody id="projectsSettingsBody">
+                                    <tr value="0">
+                                        <td>
+                                            <input
+                                                type="text"
+                                                name="projectName"
+                                                onChange={e => {this.onChangeProjectName(e)}}
                                             />
                                         </td>
-                                        <td><input
-                                                value={this.projects.projectDate}
-                                            /></td>
-                                        <td><input
-                                                value={this.projects.projectPdf}
-                                            /></td>
+                                        <td>
+                                            <input
+                                                type="text"
+                                                name="projectDate"
+                                                onChange={e => {this.onChangeProjectDate(e)}}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                type="text"
+                                                name="projectPdf"
+                                                onChange={e => {this.onChangeProjectPdf(e)}}
+                                            />
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
-                            <button onClick={e => {this.addProject(e)}}>Add Project</button>
+                            <button className="addProjectButton" onClick={e => {this.addProject(e)}}>Add Project</button>
                         </form>
 
                         <h2>Previous Clients</h2>
+                        <form>
+                            <table id="previousClientsSettings">
+                                <thead>
+                                    <tr>
+                                        <td className="clientName"><b>Client Name</b></td>
+                                        <td className="clientLocation"><b>Location</b></td>
+                                        <td className="clientWebsite"><b>Website</b></td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr value="0">
+                                        <td>
+                                            <input
+                                                onChange={e => {this.onChangeClientName(e)}}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                onChange={e => {this.onChangeClientLocation(e)}}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                onChange={e => {this.onChangeClientWebsite(e)}}
+                                            />
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <button onClick={e => {this.addClient(e)}}>Add Client</button>
+                        </form>
 
                         <h2>Qualifications</h2>
+                        <form>
+                            <table id="qualificationsSettings">
+                                <thead>
+                                    <tr>
+                                        <td className="qualificationName"><b>Qualification</b></td>
+                                        <td className="qualificationDate"><b>Date</b></td>
+                                        <td className="qualificationDocument"><b>Document</b></td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr value="0">
+                                        <td>
+                                            <input
+                                                onChange={e => {this.onChangeQualificationName(e)}}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                onChange={e => {this.onChangeQualificationDate(e)}}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                onChange={e => {this.onChangeQualificationDocument(e)}}
+                                            />
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <button onClick={e => {this.addQualification(e)}}>Add Qualification</button>
+                        </form>
 
                         <h2>Licenses & Certifications</h2>
+        
 
                         <h2>Contact</h2>
                         <form>
                             <table>
-                                <tr>
-                                    <td className="settignsCategoryPromt"><b> Phone Number: </b></td>
-                                    <td><input id="companyPhoneNumber" type="text" name="phoneNumber" value={this.contact.phoneNumber}/></td>
-                                </tr>
-                                <tr>
-                                    <td className="settignsCategoryPromt"><b> Email: </b></td>
-                                    <td><input id="companyEmail" type="text" name="email" value={this.contact.email}/></td>
-                                </tr>
-                                <tr>
-                                    <td className="settignsCategoryPromt"><b> Website: </b></td>
-                                    <td><input id="companyWebsite" type="text" name="website" value={this.contact.website}/></td>
-                                </tr>
+                                <tbody>
+                                    <tr>
+                                        <td className="settignsCategoryPromt"><b> Phone Number: </b></td>
+                                        <td>
+                                            <input
+                                                id="companyPhoneNumber"
+                                                type="text"
+                                                name="phoneNumber"
+                                                onChange={e => {this.onChangePhoneNumber(e)}}
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="settignsCategoryPromt"><b> Email: </b></td>
+                                        <td>
+                                            <input
+                                                id="companyEmail"
+                                                type="text"
+                                                name="email"
+                                                onChange={e => {this.onChangeEmail(e)}}
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="settignsCategoryPromt"><b> Website: </b></td>
+                                        <td>
+                                            <input
+                                                id="companyWebsite"
+                                                type="text"
+                                                name="website"
+                                                onChange={e => {this.onChangeWebsite(e)}}
+                                            />
+                                        </td>
+                                    </tr>
+                                </tbody>
                             </table>
                         </form>
                     </div>
